d26dab5d35ba84ad5a4c8de4a482b414c0d78a82
sohankhandaker <wadud.khandaker@selisegroup.com>
2025-06-30 00:45:37 +1200

    github action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/khcp/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
